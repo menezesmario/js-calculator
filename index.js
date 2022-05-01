ede76c4de107b1884a014ed0f90847a06df68d1f
Mário Menezes <mario082@gmail.com>
2022-05-01 22:13:30 +0000

    get values and types of buttons

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('hello')
+//selecionar calculadora
+const calculadora = document.querySelector('.calculadora')
+console.log(calculadora)
+
+//selecionar teclas
+const teclas = document.querySelector('.teclas_calculadora')
+console.log(teclas)
+
+//selecionar o valor da tecla
+teclas.addEventListener('click', evento => {
+    evento.preventDefault()
+    if(evento.target.matches('button')){
+        console.log(evento.target.innerHTML)
+    }
+
+    //definir o tipo de tecla clicada
+    const tecla = evento.target
+    const action = tecla.dataset.action
+    console.log(action)
+
+})
+
+
+
