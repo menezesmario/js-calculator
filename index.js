c199a08db0f21d85c0e38e1e9c4d03ad50bc5032
Mário Menezes <mario082@gmail.com>
2022-05-02 01:38:29 +0000

    creating calculation function

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ console.log(display)
 const teclas = document.querySelector('.teclas_calculadora')
 console.log(teclas)
 
-
+const operacao = ''
 
 //selecionar o valor da tecla
 teclas.addEventListener('click', evento => {
@@ -21,9 +21,42 @@ teclas.addEventListener('click', evento => {
     //definir o tipo de tecla clicada
     const tecla = evento.target
     const action = tecla.dataset.action
+    const conteudoTecla = tecla.textContent
+    const numeroMostrado = display.value
+
+    console.log('ct',conteudoTecla)
+
+
+    console.log('numeroMostrado', numeroMostrado)
 
     if(!action) {
         console.log('tecla numérica', evento.target.innerHTML)
+        if(numeroMostrado === '0') {
+            display.value = conteudoTecla
+        } else {
+            display.value = numeroMostrado + conteudoTecla
+        }
+    }
+
+    let primeiroNumero = ''
+
+    let segundoNumero = false
+
+
+
+    if(action) {        
+            
+    }
+
+    let operador = ''
+
+    if(action === 'adicionar') {
+        operador = '+'
+        console.log('*op', operador)
+    }
+
+    if(action === 'calcular') {
+
     }
 
     if (    action === 'adicionar' ||
@@ -31,8 +64,32 @@ teclas.addEventListener('click', evento => {
             action === 'multiplicar' ||
             action === 'dividir'
       ) {
-        console.log('tecla de operação', evento.target.innerHTML)
-      }
+        console.log(primeiroNumero)
+        console.log('2', segundoNumero)
+        primeiroNumero = numeroMostrado
+        segundoNumero = true
+        operador = evento.target.textContent
+        display.value = 0
+        console.log('2', segundoNumero)        
+    }
+
+    let resultado = ''
+
+    if (!action === 'calcular' && segundoNumero === true) {
+        segundoNumero = numeroMostrado
+
+        
+
+        if(operador === '+') {
+            resultado = primeiroNumero + segundoNumero
+        }
+
+        console.log('*resultado', resultado)
+    }
+
+    
+
+    
 })
 
 
